a901dd29cb59915cc8a2b58cd805206dd2a09787
ellieCorner <ellie25corner@gmail.com>
2025-05-27 19:23:27 -0700

    Solve the problems from ex19 to ex20

--- ex19.js
+++ ex19.js
@@ -1,7 +1,23 @@
 // Exercise 19: Write a function named 'flattenObject' that takes a nested object and returns a new object with no nested properties.
 
-function flattenObject(obj) {
-  // your code here
+function reculsiveFlat(result, current, path) {
+  for (const key in current) {
+    const resultPath = path ? `${path}.${key}` : key;
+    const value = current[key];
+
+    if (typeof value === "object") {
+      reculsiveFlat(result, value, resultPath);
+    } else {
+      result[resultPath] = value;
+    }
+  }
 }
 
-console.log(flattenObject({ a: { b: { c: 1 } }, d: 2 })) // { 'a.b.c': 1, d: 2 }
+function flattenObject(obj) {
+  const result = {};
+  reculsiveFlat(result, obj, "");
+
+  return result;
+}
+
+console.log(flattenObject({ a: { b: { c: 1 } }, d: 2 })); // { 'a.b.c': 1, d: 2 }
--- ex20.js
+++ ex20.js
@@ -1,7 +1,22 @@
 // Exercise 20: Write a function named 'anagramGroups' that takes an array of strings and returns an array of arrays, grouping anagrams together.
 
 function anagramGroups(arr) {
-  // your code here
+  if (arr.length <= 1) return arr;
+
+  const lookup = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    const originalWord = arr[i];
+    const sortedWord = arr[i].split("").sort().join("");
+
+    if (lookup[sortedWord]) {
+      lookup[sortedWord].push(originalWord);
+    } else {
+      lookup[sortedWord] = [originalWord];
+    }
+  }
+
+  return Object.values(lookup);
 }
 
-console.log(anagramGroups(['bat', 'tab', 'cat', 'act'])) // [['bat', 'tab'], ['cat', 'act']]
+console.log(anagramGroups(["bat", "tab", "cat", "act"])); // [['bat', 'tab'], ['cat', 'act']]
